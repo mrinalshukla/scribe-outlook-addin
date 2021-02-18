28b043ec677b195832345fff36ba774a898dc4af
Mrinal Shukla <mshukla@netflix.com>
2021-02-18 12:44:18 -0800

    remove unnecessary mochatest folder; will use the test folder directly.

--- test/sampleTest.js
+++ test/sampleTest.js
@@ -1,5 +1,5 @@
 var assert = require('chai').assert;
-var sample = require('../sample');
+var sample = require('../MochaTest/sample');
 
 describe('Sample', function() {
     it('sample should return hello world', function() {
