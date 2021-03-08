c4c524923f9595325945df75595878a3829c845b
CJPerez-Dev <77761058+CJPerez-Dev@users.noreply.github.com>
2021-03-07 17:55:50 -0700

    Update signaturelist.js
    Created two functions within signaturelist.js . One function takes in the new signature parameters and adds a new key, value pair to the database. It also overwrites an existing key. The second function removes a signature from the database if it exists.

--- src/signaturelist/signaturelist.js
+++ src/signaturelist/signaturelist.js
@@ -6,5 +6,21 @@ module.exports = {
     returnQuote: function(Quote1) {
     var quote  = signatureDataBase[Quote1];
     return quote;
+    },
+
+    addSignature: function(signatureDB, signatureTitle, signatureContents) {
+        //This will create a new key, value pair if it does not exist or overwrite an existing
+        //key, value pair if already in database
+        signatureDB[signatureTitle] = signatureContents;
+    },
+    
+    removeSignature: function(signatureDB, signatureTitle) {
+        //This will remove a key, value pair from the database if it exists
+        if(signatureDB.hasOwnProperty(signatureTitle)) {
+        delete signatureDB[signatureTitle];
+            }
+        else {
+            console.log("There is no existing item with the provided title.");
+        }
     }
 }
