dfcbad8a5f0af380354dbcd0f1ef5fd8f038ed99
Juan Ruiz <jruiz50@msudenver.edu>
2021-03-08 18:11:50 -0700

    Revised the contents in the dictionary.  Now there are real quotes in the dictionary.  Also, there are tests to demonstrate that the dictionary is holding the proper information.

--- test/signaturelistTest.js
+++ test/signaturelistTest.js
@@ -2,22 +2,29 @@ const assert = require('chai').assert;
 const returnQuote = require('../src/signaturelist/signaturelist.js').returnQuote;
 
 describe('Dictionary', function() {
-    it('Returns string', function() {
-        let quote = returnQuote("Signature");
+    it('Returns a string', function() {
+        let quote = returnQuote('Steve Jobs');
         assert.typeOf(quote, "string");
     });
 });
 
 describe('Dictionary', function() {
-    it('Returns value for specified key', function() {
-        let quote = returnQuote("Signature");
-        assert.equal(quote, "Quote");
+    it('Returns a Steve Jobs quote', function() {
+        let quote = returnQuote('Steve Jobs');
+        assert.equal(quote, '"Stay hungry, stay foolish." -Steve Jobs');
     });
 });
 
 describe('Dictionary', function() {
-    it('Returns value for specified key', function() {
-        let quote = returnQuote("Einstein");
-        assert.equal(quote, "Time");
+    it('Returns a JFK quote', function() {
+        let quote = returnQuote('JFK');
+        assert.equal(quote, '"Those who dare to fail miserably can achieve greatly." -John F. Kennedy');
+    });
+});
+
+describe('Dictionary', function() {
+    it('Returns a Plato quote', function() {
+        let quote = returnQuote('Plato');
+        assert.equal(quote, '"The greatest wealth is to live content with little." - Plato');
     });
 });
