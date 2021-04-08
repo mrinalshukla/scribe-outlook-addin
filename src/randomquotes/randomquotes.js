a1fef7f0fb76b4be4348ef395c24ed5f3cffa466
Mrinal Shukla <mshukla@netflix.com>
2021-04-07 21:02:47 -0700

    Prettyfied the mocha test console output. Fixed minor naming errors in randomquotes module.

--- src/randomquotes/randomquotes.js
+++ src/randomquotes/randomquotes.js
@@ -1,8 +1,7 @@
-//import * as randomQuotes from "random-quotes";
 const randomQuotes = require('random-quotes');
 
 function getQuotes (howMany) {
     return randomQuotes.default(howMany);
 }
 
-module.exports.default = getQuotes
+module.exports.getQuotes = getQuotes
