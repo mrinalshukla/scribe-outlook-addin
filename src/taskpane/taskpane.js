bf0fa0a3d800fdcaee817d767ea6cf37594a0a8b
CJPerez-Dev <77761058+CJPerez-Dev@users.noreply.github.com>
2021-04-12 22:41:58 -0600

    Current progress on App UI rework
    Not entirely finished, but wanted to commit just in case anyone else is relying on these. I have the majority of the fields and buttons setup. We now just need to tie the JS functionality to each button.

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -7,7 +7,6 @@
 import "../../assets/icon-16.png";
 import "../../assets/icon-32.png";
 import "../../assets/icon-80.png";
-import { displaySignatures } from "../signaturedropdown/signaturedropdown";
 
 /* global document, Office */
 
